bf04263072177670e79dbbe6dbbd13324b294bc8
hcaban <hectorcbn03@gmail.com>
2023-11-05 12:37:18 -0500

    Created research-dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,6 +19,7 @@ import StudentLogin from './views/StudentLogin/StudentLogin';
 import ForgetPassword from './views/TeacherLogin/ForgetPassword';
 import ResetPassword from './views/TeacherLogin/ResetPassword';
 import TeacherLogin from './views/TeacherLogin/TeacherLogin';
+import ResearchDashboard from './views/Researcher/Dashboard/ResearchDashboard';
 
 const App = () => {
   return (
@@ -32,6 +33,14 @@ const App = () => {
         <Route path='/login' element={<StudentLogin />} />
         <Route path='/replay/:saveID' element={<Replay />} />
         <Route path='/sandbox' element={<BlocklyPage isSandbox={true} />} />
+        <Route
+          path='/research-dashboard'
+          element={
+            <PrivateRoute>
+              <ResearchDashboard />
+            </PrivateRoute>
+          }
+        />
         <Route
           path='/report'
           element={
