7ef4bde8732ca6a95b54e34a97a5fab3eb8a2568
Jacob Leverock <greenops@comcast.net>
2023-12-01 14:06:13 -0500

    Final updated studies implementation

--- client/src/views/Researcher/Study.jsx
+++ client/src/views/Researcher/Study.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { getMentor, getClassrooms } from '../../Utils/requests';
+import { getMentor, getstudies } from '../../Utils/requests';
 import { message } from 'antd';
 import './Study.less';
 import DashboardDisplayCodeModal from '../Mentor/Dashboard/DashboardDisplayCodeModal';
@@ -9,60 +9,60 @@ import { useGlobalState } from '../../Utils/userState';
 import { useNavigate } from 'react-router-dom';
 
 export default function Study() {
-  const [studies, setStudies] = useState([]);
-  const [value] = useGlobalState('currUser');
-  const navigate = useNavigate();
+    const [studies, setstudies] = useState([]);
+    const [value] = useGlobalState('currUser');
+    const navigate = useNavigate();
 
-  useEffect(() => {
-      let studyids = [];
-      getResearcher().then((res) => {
-          if (res.data) {
-              res.data.studies.forEach((study) => {
-                  studyids.push(study.id);
-              });
-              getStudies(studyids).then((studies) => {
-                  setStudies(studies);
-              });
-          } else {
-              message.error(res.err);
-              navigate('/teacherlogin');
-          }
-      });
-  }, []);
+    useEffect(() => {
+        let studyIDs = [];
+        getMentor().then((res) => {
+            if (res.data) {
+                res.data.studies.forEach((study) => {
+                    studyIDs.push(study.id);
+                });
+                getstudies(studyIDs).then((studies) => {
+                    setstudies(studies);
+                });
+            } else {
+                message.error(res.err);
+                navigate('/teacherlogin');
+            }
+        });
+    }, []);
 
-  const handleViewStudy = (studyId) => {
-      navigate(`/study/${studyId}`);
-  };
+    const handleViewstudy = (studyID) => {
+        navigate(`/study/${studyID}`);
+    };
 
-  return (
-      <div className='container nav-padding'>
-          <NavBar />
-          <div id='main-header'>Welcome {value.name}</div>
-          <MentorSubHeader title={'Your studies'}></MentorSubHeader>
-          <div id='studies-container'>
-              <div id='dashboard-card-container'>
-                  {studies.map((study) => (
-                      <div key={study.id} id='dashboard-class-card'>
-                          <div id='card-left-content-container'>
-                              <h1 id='card-title'>{study.name}</h1>
-                              <div id='card-button-container' className='flex flex-row'>
-                                  <button onClick={() => handleViewstudy(study.id)}>
-                                      View
-                                  </button>
-                              </div>
-                          </div>
-                          <div id='card-right-content-container'>
-                              <DashboardDisplayCodeModal code={study.code} />
-                              <div id='divider' />
-                              <div id='student-number-container'>
-                                  <h1 id='number'>{study.students.length}</h1>
-                                  <p id='label'>Students</p>
-                              </div>
-                          </div>
-                      </div>
-                  ))}
-              </div>
-          </div>
-      </div>
-  );
+    return (
+        <div className='container nav-padding'>
+            <NavBar />
+            <div id='main-header'>Welcome {value.name}</div>
+            <MentorSubHeader title={'Your studies'}></MentorSubHeader>
+            <div id='studies-container'>
+                <div id='dashboard-card-container'>
+                    {studies.map((study) => (
+                        <div key={study.id} id='dashboard-class-card'>
+                            <div id='card-left-content-container'>
+                                <h1 id='card-title'>{study.name}</h1>
+                                <div id='card-button-container' className='flex flex-row'>
+                                    <button onClick={() => handleViewstudy(study.id)}>
+                                        View
+                                    </button>
+                                </div>
+                            </div>
+                            <div id='card-right-content-container'>
+                                <DashboardDisplayCodeModal code={study.code} />
+                                <div id='divider' />
+                                <div id='student-number-container'>
+                                    <h1 id='number'>{study.students.length}</h1>
+                                    <p id='label'>Students</p>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 }
